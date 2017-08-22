620d4834bc8f9c6b9e8eecd962aa23ac0a5a3593
wangyafei <wangyafei@startimes.com.cn>
2017-08-22 18:48:30 +0800

    modify eventhelper.js

---
+++ test/index.js
@@ -0,0 +1,22 @@
+import EventHelper from '../lib/eventhelper';
+
+let emmiter = new EventHelper();
+emmiter.on('hello', (name)=> console.log(`hello ${name}`));
+
+emmiter.emit('hello', 'wangyafei')
+
+setTimeout(()=>{
+    emmiter.emit('read','read book 10')
+}, 1000);
+
+let times = 0;
+setTimeout(()=>{
+    times++;
+    emmiter.emit('write', `write book ${times}`)
+}, 500);
+
+emmiter.all('read', 'write', (readResult, writeResult)=> console.log(`read: ${readResult} and write: ${writeResult}`));
+
+emmiter.race('read', 'write', (result)=> console.log(`the winner is: ${result}`));
+
+emmiter.tail('read', 'write', (readResult, writeResult)=> console.log(`result is: ${readResult} -- ${writeResult}`))
