c20259a3084f4b1ad20f9fd008538b4b30545d20
wangyafei <wangyafei@startimes.com.cn>
2017-09-11 17:38:05 +0800

    add ract-test and tail-test

---
+++ test/race-test.js
@@ -0,0 +1,27 @@
+let assert = require('assert');
+let fs = require('mz/fs');
+let EventHelper = require('../index');
+
+describe('#method: race', ()=>{
+    let emmiter = new EventHelper();
+
+    it('#race() should trriger the handler with value {eventType: "read", data: "read10"}', (done)=>{
+        emmiter.race('read', 'readMore', ({eventType, data})=>{
+            assert.equal(eventType, 'read');
+            assert.equal(data, 'read10');
+            done();
+        });
+        fs.readFile('./mock/read.txt', 'utf-8', (err, data)=> emmiter.emit('read', data));
+        fs.readFile('./mock/readMore.txt', 'utf-8', (err, data)=> emmiter.emit('readMore', data));
+    });
+
+    it('#race() should trriger the handler with value {eventType: "time300", data: 300}', (done)=>{
+        emmiter.race('time300', 'time500', ({eventType, data})=>{
+            assert.equal(eventType, 'time300');
+            assert.equal(data, 300);
+            done();
+        });
+        setTimeout(()=> emmiter.emit('time300', 300), 300);
+        setTimeout(()=> emmiter.emit('time500', 500), 500);
+    });
+});
