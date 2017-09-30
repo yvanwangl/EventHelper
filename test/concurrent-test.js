7d1ba0f6a2b62ad246edd0a568f4a3b74d09b081
yvanwangl <1012305328@qq.com>
2017-09-30 22:00:29 +0800

    modify concurrent-test

--- test/concurrent-test.js
+++ test/concurrent-test.js
@@ -1,4 +1,5 @@
 let assert = require('assert');
+//let {Image} = require('canvas');
 let EventHelper = require('../index');
 let images = require('../mock/images');
 
@@ -6,12 +7,12 @@ describe('#method: concurrent', ()=>{
     let emmiter = new EventHelper();
     it('#concurrent(eventType, limit, asyncHandler, params)', (done)=> {
         let loadImg = (url, callback)=>{
-            let img = new Image();
+            let img = document.createElement('img');
             img.onload = ()=> callback(null, img);
             img.onerror = (error)=> callback(error, null);
             img.src = url;
         };
-        emmiter.concurrent('load', 5, loadImg, []);
+        emmiter.concurrent('load', 5, loadImg, images);
         emmiter.on('finish', (result)=> {
             assert.equal(result.length, 20);
             done();
