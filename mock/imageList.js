ab3b5c666c3e6460c5ffa7c3d3e4b0a1d95a6069
yvanwangl <1012305328@qq.com>
2017-10-03 14:48:41 +0800

    add concurrent method and unit test

---
+++ mock/imageList.js
@@ -0,0 +1,24 @@
+define({
+    images: [
+        "https://www.baidu.com/img/bd_logo1.png", 
+        "https://www.baidu.com/img/baidu_jgylogo3.gif",
+        "https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=265704898,674087460&fm=58", 
+        "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3105518379,1723849651&fm=58", 
+        "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2292095202,1784829557&fm=58", 
+        "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1219708893,1812014204&fm=58",
+        "https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3669175269,2356096679&fm=58", 
+        "https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=154063165,2016512383&fm=202&mola=new&crop=v1", 
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3536541845,739399450&fm=27&gp=0.jpg", 
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=594559231,2167829292&fm=27&gp=0.jpg", 
+        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3138365389,851751545&fm=27&gp=0.jpg", 
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3965705221,2010595691&fm=27&gp=0.jpg", 
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1742626185,2547278809&fm=27&gp=0.jpg", 
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1060387669,1498970204&fm=27&gp=0.jpg", 
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=4271053251,2424464488&fm=27&gp=0.jpg", 
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=4140230371,1584894266&fm=27&gp=0.jpg", 
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2710025438,2030209695&fm=27&gp=0.jpg", 
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3432487329,2901563519&fm=27&gp=0.jpg", 
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2993396273,3023277058&fm=27&gp=0.jpg", 
+        "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2838934065,571280381&fm=27&gp=0.jpg"
+    ]
+});
--- test/concurrent-test.js
+++ test/concurrent-test.js
@@ -1,21 +1,30 @@
-let assert = require('assert');
-//let {Image} = require('canvas');
-let EventHelper = require('../index');
-let images = require('../mock/images');
 
-describe('#method: concurrent', ()=>{
-    let emmiter = new EventHelper();
-    it('#concurrent(eventType, limit, asyncHandler, params)', (done)=> {
-        let loadImg = (url, callback)=>{
-            let img = document.createElement('img');
-            img.onload = ()=> callback(null, img);
-            img.onerror = (error)=> callback(error, null);
-            img.src = url;
-        };
-        emmiter.concurrent('load', 5, loadImg, images);
-        emmiter.on('finish', (result)=> {
-            assert.equal(result.length, 20);
-            done();
+requirejs.config({
+    baseUrl: 'vendor',
+    paths: {
+        'eventhelper': '../../lib/eventhelper'
+    }
+});
+
+require(['chai', 'eventhelper', '../../mock/imageList','mocha'], function(chai, EventHelper, imageList){
+    mocha.setup('bdd');
+    let expect = chai.expect;
+    describe('#method: concurrent', ()=>{
+        let emmiter = new EventHelper();
+        it('#concurrent(eventType, limit, asyncHandler, params)', (done)=> {
+            let loadImg = (url, callback)=>{
+                let img = document.createElement('img');
+                img.onload = ()=> callback(null, img);
+                img.onerror = (error)=> callback(error, null);
+                img.src = url;
+            };
+            emmiter.concurrent('load', 5, loadImg, imageList.images);
+            emmiter.on('loadFinish', (result)=> {
+                expect(result.length).to.equal(20);
+                done();
+            });
         });
     });
+    mocha.run();
 });
+
